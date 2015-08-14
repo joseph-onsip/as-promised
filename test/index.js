04e416a3fd23991a30772474985f0d6f66c10a3f
Joseph Frazier <joseph@onsip.com>
2015-08-14 10:28:45 -0400

    test: add ability to check `this` as well

--- test/index.js
+++ test/index.js
@@ -2,10 +2,13 @@ import 'native-promise-only'
 import test from 'tape'
 import asPromised from '../'
 
-function check (name, expected, args) {
+function check (name, expected, args, expectedThis, givenThis) {
   test(name, function (t) {
-    t.plan(1);
-    asPromised((...actual) => t.deepEqual(actual, expected)).apply(null, args)
+    t.plan(2);
+    asPromised(function (...actual) {
+      t.deepEqual(actual, expected);
+      t.deepEqual(this, expectedThis);
+    }).apply(givenThis, args);
   });
 }
 
